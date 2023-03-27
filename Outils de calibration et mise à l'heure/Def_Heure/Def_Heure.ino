655ea2a2701064d17346ce9878ed1729fc9f08a5
cballevre <celestin.ballevre@protonmail.com>
2023-03-27 20:12:17 +0200

    feat: Add program to test and calibrate

---
+++ Outils de calibration et mise à l'heure/Def_Heure/Def_Heure.ino
@@ -0,0 +1,62 @@
+
+// Pour horloge RTC
+#include <DS3231.h>
+
+DS3231 Clock;
+bool Century = false;
+bool h12;
+bool PM;
+byte ADay, AHour, AMinute, ASecond, ABits;
+bool ADy, A12h, Apm;
+
+byte year, month, date, DoW, hour, minute, second;
+int state = 1; // 0 pour set time, 1 pour get time
+
+int donnee; // données reçues sur la liaison série
+
+void setup() {
+  Serial.begin(115200);
+
+  // seulement si besoin avec alim commandée
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+  
+  // put your setup code here, to run once:
+  //Initilisation wire pour rtc
+  Wire.begin();
+
+}
+
+void loop() {
+  Serial.println("presser entrer pour lancer la synchro");
+  if (Serial.available() > 0) {
+    // on lit la données reçue (dans la variable "donnee")
+    donnee = Serial.read();
+    // set time
+    Clock.setSecond(00);//Set the second
+    Clock.setMinute(11);//Set the minute
+    Clock.setHour(14);  //Set the hour
+    Clock.setDoW(01);    //Set the day of the week
+    Clock.setDate(27);  //Set the date of the month
+    Clock.setMonth(3);  //Set the month of the year
+    Clock.setYear(23);  //Set the year (Last two digits of the year)
+    Clock.setClockMode(false); // pour avoir le format 24h
+    Serial.println("synchro ok");
+  }
+  else {
+    int second, minute, hour, date, month, year;
+
+    // lecture de l'horloge rtc
+    second = Clock.getSecond();
+    minute = Clock.getMinute();
+    hour = Clock.getHour(h12, PM);
+    date = Clock.getDate();
+    month = Clock.getMonth(Century);
+    year = Clock.getYear();
+    Serial.println("");
+    Serial.print(year); Serial.print("/"); Serial.print(month); Serial.print("/"); Serial.print(date); Serial.print(" ; ");
+    Serial.print(hour); Serial.print(" : "); Serial.print(minute); Serial.print(" : "); Serial.println(second);
+    delay(1000);
+  }
+
+}
